b208878f3c26e59b0dadd701036a6a1bb807d346
LSH <linsihang@jimitc.com>
2021-02-16 10:13:30 +0800

    抽取公共代码 common-api

--- cloud-common-api/src/main/java/my/lsh/cloud/entities/PaymentEntity.java
+++ cloud-common-api/src/main/java/my/lsh/cloud/entities/PaymentEntity.java
@@ -3,12 +3,14 @@ package my.lsh.cloud.entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class PaymentEntity implements Serializable {
 
     private Long id;

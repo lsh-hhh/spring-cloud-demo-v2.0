4fcd6076d8253edb8aa6e7a95814a728405af3d4
LSH <linsihang@jimitc.com>
2021-02-16 16:32:00 +0800

    使用微服务名称替代具体ip地址； restTemplate添加@LoadBalence注解

--- cloud-consumer-payment-9001/src/main/java/my/lsh/cloud/CloudConsumerPayment_9001.java
+++ cloud-consumer-payment-9001/src/main/java/my/lsh/cloud/CloudConsumerPayment_9001.java
@@ -2,7 +2,9 @@ package my.lsh.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class CloudConsumerPayment_9001 {
     public static void main(String[] args) {
--- cloud-consumer-payment-9001/src/main/java/my/lsh/cloud/controller/ConsumerPaymentController.java
+++ cloud-consumer-payment-9001/src/main/java/my/lsh/cloud/controller/ConsumerPaymentController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import my.lsh.cloud.entities.CommonResult;
 import my.lsh.cloud.entities.PaymentEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,18 +15,21 @@ public class ConsumerPaymentController {
 
     @Autowired
     private RestTemplate restTemplate;
-    private final String BASE_URL = "http://localhost:8001/payment";
+
+//    private final String BASE_URL = "http://localhost:8001/payment";
+    private final String BASE_URL = "http://CLOUD-PROVIDER-PAYMENT";
 
     @GetMapping("/add")
     public CommonResult create(PaymentEntity paymentEntity) {
-        log.info("添加的实例：" + paymentEntity);
-        String url = BASE_URL + "/add";
+        String url = BASE_URL + "/payment/add";
+        log.info("添加的实例：" + paymentEntity + " 请求地址：" + url);
         return restTemplate.postForObject(url, paymentEntity, CommonResult.class);
     }
 
     @GetMapping("/get/{id}")
-    public CommonResult getPaymentById(@PathVariable("id") Integer id) {
-        String url = BASE_URL + "/get/" + id;
+    public CommonResult getPaymentById(@PathVariable("id") Long id) {
+        String url = BASE_URL + "/payment/get/" + id;
+        log.info("请求id：" + id + " 请求地址：" + url);
         return restTemplate.getForObject(url, CommonResult.class);
     }
 }

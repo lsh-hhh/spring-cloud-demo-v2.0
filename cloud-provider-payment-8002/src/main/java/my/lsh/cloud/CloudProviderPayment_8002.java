4413496b494157ed520fc70b49476e450a48e944
LSH <linsihang@jimitc.com>
2021-02-18 00:48:33 +0800

    添加provider集群

---
+++ cloud-provider-payment-8002/src/main/java/my/lsh/cloud/CloudProviderPayment_8002.java
@@ -0,0 +1,15 @@
+package my.lsh.cloud;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+@MapperScan("my.lsh.cloud.dao")
+public class CloudProviderPayment_8002 {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudProviderPayment_8002.class, args);
+    }
+}

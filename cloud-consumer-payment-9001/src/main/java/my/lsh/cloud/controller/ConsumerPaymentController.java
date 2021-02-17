ea58c48bc5da9e91687c69aaaa0ea56d11506065
LSH <linsihang@jimitc.com>
2021-02-18 00:49:20 +0800

    优化

--- cloud-consumer-payment-9001/src/main/java/my/lsh/cloud/controller/ConsumerPaymentController.java
+++ cloud-consumer-payment-9001/src/main/java/my/lsh/cloud/controller/ConsumerPaymentController.java
@@ -4,8 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import my.lsh.cloud.entities.CommonResult;
 import my.lsh.cloud.entities.PaymentEntity;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
@@ -15,21 +18,29 @@ public class ConsumerPaymentController {
 
     @Autowired
     private RestTemplate restTemplate;
+    @Value("${server.port}")
+    private String serverPort;
 
-//    private final String BASE_URL = "http://localhost:8001/payment";
-    private final String BASE_URL = "http://CLOUD-PROVIDER-PAYMENT";
+    private final String BASE_URL = "http://cloud-provider-payment";
 
     @GetMapping("/add")
     public CommonResult create(PaymentEntity paymentEntity) {
         String url = BASE_URL + "/payment/add";
-        log.info("添加的实例：" + paymentEntity + " 请求地址：" + url);
+        log.info("添加的实例：" + paymentEntity + " 请求地址：" + url + "\t" + "端口号：" + serverPort);
         return restTemplate.postForObject(url, paymentEntity, CommonResult.class);
     }
 
     @GetMapping("/get/{id}")
     public CommonResult getPaymentById(@PathVariable("id") Long id) {
         String url = BASE_URL + "/payment/get/" + id;
-        log.info("请求id：" + id + " 请求地址：" + url);
+        log.info("请求id：" + id + " 请求地址：" + url + "\t" + "端口号：" + serverPort);
+        return restTemplate.getForObject(url, CommonResult.class);
+    }
+
+    @GetMapping("/discovery")
+    public CommonResult discovery(){
+        String url = BASE_URL + "/payment/discovery";
+        log.info(" 请求地址：" + url + "\t" + "端口号：" + serverPort);
         return restTemplate.getForObject(url, CommonResult.class);
     }
 }
--- cloud-provider-payment-8001/src/main/java/my/lsh/cloud/controller/PaymentController.java
+++ cloud-provider-payment-8001/src/main/java/my/lsh/cloud/controller/PaymentController.java
@@ -5,7 +5,12 @@ import my.lsh.cloud.entities.CommonResult;
 import my.lsh.cloud.entities.PaymentEntity;
 import my.lsh.cloud.service.PaymentServiceI;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/payment")
@@ -14,13 +19,17 @@ public class PaymentController {
 
     @Autowired
     private PaymentServiceI paymentService;
+    @Value("${server.port}")
+    private String serverPort;
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @PostMapping("/add")
     public CommonResult create(@RequestBody PaymentEntity paymentEntity) {
         log.info("添加的实例：" + paymentEntity);
         Integer i = paymentService.create(paymentEntity);
         if(i > 0){
-            log.info("******添加成功！");
+            log.info("******添加成功，端口号为:" + serverPort);
             return new CommonResult(200, "添加成功！", i);
         }
         return new CommonResult(444, "添加失败！", 0);
@@ -31,9 +40,18 @@ public class PaymentController {
         PaymentEntity payment = paymentService.getPaymentById(id);
 
         if (payment != null) {
-            log.info("******查询成功，结果：" + payment);
+            log.info("******查询成功，端口号为:" + serverPort + "， 结果：" + payment);
             return new CommonResult(200, "查询成功！", payment);
         }
         return new CommonResult(444, "查询失败，无此id：" + id + "的记录！", payment);
+    }
+
+    @GetMapping("/discovery")
+    public CommonResult discovery(){
+        List<String> services = discoveryClient.getServices();
+        log.info("所有注册的服务：" + services);
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("cloud-provider-payment");
+        return new CommonResult(200, "微服务cloud-provider-payment信息", instances);
     }
 }

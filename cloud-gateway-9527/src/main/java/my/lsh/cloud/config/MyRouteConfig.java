fa2a88dc730c20278cea14be51d3fda46eddc64f
LSH <linsihang@jimitc.com>
2021-02-20 09:24:34 +0800

    添加路由网关gateway

---
+++ cloud-gateway-9527/src/main/java/my/lsh/cloud/config/MyRouteConfig.java
@@ -0,0 +1,18 @@
+package my.lsh.cloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyRouteConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder){
+
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        routes.route("customRoute_path", r -> r.path("/guonei").uri("http://news.baidu.com/guonei")).build();
+        return routes.build();
+    }
+}

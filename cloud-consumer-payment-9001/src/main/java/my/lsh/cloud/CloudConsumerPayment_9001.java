063408d3228856a4490522056baf15ec55faeb81
LSH <linsihang@jimitc.com>
2021-02-20 09:30:41 +0800

    整合openFeign

--- cloud-consumer-payment-9001/src/main/java/my/lsh/cloud/CloudConsumerPayment_9001.java
+++ cloud-consumer-payment-9001/src/main/java/my/lsh/cloud/CloudConsumerPayment_9001.java
@@ -3,7 +3,9 @@ package my.lsh.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication
 public class CloudConsumerPayment_9001 {

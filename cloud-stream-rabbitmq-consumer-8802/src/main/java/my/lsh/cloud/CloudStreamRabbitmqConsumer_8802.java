eed081c8e2b26276e0c2a983308658bd85ece8ca
LSH <lshandhsl@outlook.com>
2021-02-21 17:05:53 +0800

    消息驱动模块stream

---
+++ cloud-stream-rabbitmq-consumer-8802/src/main/java/my/lsh/cloud/CloudStreamRabbitmqConsumer_8802.java
@@ -0,0 +1,11 @@
+package my.lsh.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudStreamRabbitmqConsumer_8802 {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudStreamRabbitmqConsumer_8802.class, args);
+    }
+}

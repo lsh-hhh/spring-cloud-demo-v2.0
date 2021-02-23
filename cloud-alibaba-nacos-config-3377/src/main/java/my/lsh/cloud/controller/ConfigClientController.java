2eb064858669e370ba8a4d368f95e909b83562b1
LSH <lshandhsl@outlook.com>
2021-02-23 16:49:45 +0800

    nacos配置中心测试demo

---
+++ cloud-alibaba-nacos-config-3377/src/main/java/my/lsh/cloud/controller/ConfigClientController.java
@@ -0,0 +1,19 @@
+package my.lsh.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @RequestMapping("/getClientInfo")
+    public String getClientInfo() {
+        return "" + configInfo;
+    }
+}
